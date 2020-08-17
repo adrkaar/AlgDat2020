df5e72c0b9162acabd3faf9084106e30248bb61e
Adrian Karlsen <54044745+adrkaar@users.noreply.github.com>
2020-08-17 14:24:47 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.print("Teller til ti");
+        for (int i=0; i<=10; ++i){
+            System.out.println(i);
+        }
+    }
+}
